9a7e9e0503dcf24e13b43227441e8653256be7ae
gherron94 <140562713+gherron94@users.noreply.github.com>
2024-02-07 13:33:10 +0000

    vote on article

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -7,25 +7,45 @@ export default function SingleArticle() {
 
   const { article_id } = useParams()
   const [singleArticle, setSingleArticle] = useState({});
+  const [voteCount, setVoteCount] = useState(0)
 
   useEffect(() => {
     newsApi.get(`/articles/${article_id}`)
     .then(({data}) => {
-      setSingleArticle(data.article)  
+      setSingleArticle(data.article)
+      setVoteCount(data.article.votes)
     })
   }, [])
 
+  function upVote() {
+    setVoteCount(voteCount + 1)
+    newsApi.patch(`/articles/${article_id}`, {
+     "inc_votes": 1})
+  }
+
+  function downVote() {
+    setVoteCount(voteCount - 1)
+    newsApi.patch(`/articles/${article_id}`, {
+      "inc_votes": -1})
+  }
+
   return (
     <>
-     <div className='single-article'>
+    <div className='single-article'>
+    <div className='article-info'>
         <h3>{singleArticle.title}</h3>
-        <p>Topic: {singleArticle.topic}</p>
         <p>By: {singleArticle.author}</p>
+        <p>Topic: {singleArticle.topic}</p>
         <p>{singleArticle.body}</p>
-        <p>Total Votes: {singleArticle.votes}</p>
+        <p>Total Votes: {voteCount}</p>
         <p>Date created: {`${new Date(singleArticle.created_at)}`}</p>
       </div>
-      {<Comments></Comments>}
+      <div className='vote-buttons'>
+      <button onClick={upVote} className='vote-button'>upvote</button>
+      <button onClick={downVote} className='vote-button'>downvote</button>
+      </div>
+    </div>
+    {<Comments></Comments>}
       </>
    )
 }
