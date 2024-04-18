d93ec02f96e660702a7e5c65915300827f8ce91d
gherron94 <140562713+gherron94@users.noreply.github.com>
2024-04-18 21:18:48 +0100

    comment styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,71 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import NavBar from './components/NavBar'
-import Home from './components/Home'
-import Articles from './components/Articles'
-import Users from './components/Users'
-import SingleArticle from './components/SingleArticle'
-import { useState } from 'react'
-import UserContext from './components/UserContext'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Articles from "./components/Articles";
+import Users from "./components/Users";
+import SingleArticle from "./components/SingleArticle";
+import { useState, useEffect } from "react";
+import UserContext from "./components/UserContext";
 
 function App() {
+	const [isTopics, setIsTopics] = useState(false);
 
-  const [isTopics, setIsTopics] = useState(false)
+	let user = JSON.parse(localStorage.getItem("users") ?? "{}");
 
-  const [signedInUser, setSignedInUser] = useState({
-    username: 'tickle122',
-    avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953'
- })
+	if (!user.username) {
+		user = {
+			username: "tickle122",
+			avatar_url:
+				"https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
+		};
+	}
 
-  return (
-    <>
-  <UserContext.Provider value={{signedInUser, setSignedInUser}}>
-  <div className={isTopics ? "container-add-topics" : "container"}>
-    <div className="header">
-      <header>
-      <Header/>
-      <NavBar isTopics={isTopics} setIsTopics={setIsTopics}/>
-      </header>
-    </div>
-    <div className="body">
-      <div className="content">
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/articles" element={<Articles/>} />
-            <Route path="/articles/:article_id" element={<SingleArticle />} />
-            <Route path={`/articles/topics/:topic_id`} element={<Articles/>} />
-            <Route path="/users" element={<Users/>} />
-          </Routes>
-        </main>
-        <footer className='footer'>
-          <p>© 2024 NC news</p>
-        </footer>
-      </div>
-  </div>
-</div>
-</UserContext.Provider>
-    </>
-  )
+	console.log(user);
+
+	const [signedInUser, setSignedInUser] = useState(user);
+
+	useEffect(() => {
+		localStorage.setItem("users", JSON.stringify(signedInUser));
+	}, [signedInUser]);
+
+	return (
+		<>
+			<UserContext.Provider value={{ signedInUser, setSignedInUser }}>
+				<div className={isTopics ? "container-add-topics" : "container"}>
+					<div className="header">
+						<header>
+							<Header />
+							<NavBar isTopics={isTopics} setIsTopics={setIsTopics} />
+						</header>
+					</div>
+					<div className="body">
+						<div className="content">
+							<main>
+								<Routes>
+									<Route path="/" element={<Home />} />
+									<Route path="/articles" element={<Articles />} />
+									<Route
+										path="/articles/:article_id"
+										element={<SingleArticle />}
+									/>
+									<Route
+										path={`/articles/topics/:topic_id`}
+										element={<Articles />}
+									/>
+									<Route path="/users" element={<Users />} />
+								</Routes>
+							</main>
+							<footer className="footer">
+								<p>© 2024 NC news</p>
+							</footer>
+						</div>
+					</div>
+				</div>
+			</UserContext.Provider>
+		</>
+	);
 }
 
-export default App
-
+export default App;
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,52 +1,68 @@
-import {useState} from 'react'
-import newsApi from './api'
+import { useState } from "react";
+import newsApi from "./api";
 
-import { useContext } from 'react';
-import UserContext from "./UserContext"
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
+export default function Users() {
+	const { signedInUser, setSignedInUser } = useContext(UserContext);
+	const [usersList, setUsersList] = useState([]);
+	const [isLoadingUsers, setIsLoadingUsers] = useState(true);
 
-  export default function Users() {
+	newsApi.get("/users").then(({ data }) => {
+		setIsLoadingUsers(false);
+		setUsersList(data.users);
+	});
 
+	function switchUser(event) {
+		const username = event.target.parentNode.childNodes[0].innerText;
+		const avatar_url = event.target.parentNode.childNodes[1].src;
+		setSignedInUser((currSignedInUser) => {
+			return { ...currSignedInUser, username, avatar_url };
+		});
+	}
 
-  const {signedInUser, setSignedInUser} = useContext(UserContext)
-  const [usersList, setUsersList]= useState([])
-  const [isLoadingUsers, setIsLoadingUsers] = useState(true)
-
-  newsApi.get('/users')
-      .then(({data}) => {
-        setIsLoadingUsers(false)
-        setUsersList(data.users)
-      })
-  
-  function switchUser(event) {
-    const username = event.target.parentNode.childNodes[0].innerText;
-    const avatar_url = event.target.parentNode.childNodes[1].src;
-    setSignedInUser((currSignedInUser) => {
-        return {...currSignedInUser, username, avatar_url}
-    })
-  }
-
-return (
-  <>
-  {isLoadingUsers ? <div className='loading' ><h3>Loading Users...</h3></div> : 
-  <>
- <h2>Select User</h2>
- <div className="flex-box-user"> 
-      <ul>
-        {usersList.map(user => {
-          return (
-          <li className={user.username !== signedInUser.username ? 'signed-out-card':'signed-in-card'} key={user.username} >
-               <h3>{user.username}</h3>
-               <img src={user.avatar_url} alt={`An image of ${user.username}'s avatar`}/>
-               {console.log()}
-               {user.username !== signedInUser.username ?   <button onClick={switchUser} >Select User</button> :<p><strong>Signed In</strong></p>}
-            </li>)
-        })}
-      </ul>
-    </div>
-    </>
-  }
-  </>
-)
+	return (
+		<>
+			{isLoadingUsers ? (
+				<div className="loading">
+					<h3>Loading Users...</h3>
+				</div>
+			) : (
+				<>
+					<h2>Select User</h2>
+					<div className="flex-box-user">
+						<ul>
+							{usersList.map((user) => {
+								return (
+									<li
+										className={
+											user.username !== signedInUser.username
+												? "signed-out-card"
+												: "signed-in-card"
+										}
+										key={user.username}
+									>
+										<h3>{user.username}</h3>
+										<img
+											src={user.avatar_url}
+											alt={`An image of ${user.username}'s avatar`}
+										/>
+										{console.log()}
+										{user.username !== signedInUser.username ? (
+											<button onClick={switchUser}>Select User</button>
+										) : (
+											<p>
+												<strong>Signed In</strong>
+											</p>
+										)}
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+				</>
+			)}
+		</>
+	);
 }
-
