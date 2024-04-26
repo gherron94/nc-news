25bae582a58722fc48ceee6fc332077bb0113209
gherron94 <140562713+gherron94@users.noreply.github.com>
2024-04-26 17:58:14 +0100

    style slide animation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -41,11 +41,9 @@ export default function NavBar({ isTopics, setIsTopics }) {
 						<li onClick={chooseTopic}>
 							<NavLink to="articles/topics/cooking"> Cooking</NavLink>
 						</li>
-						<div className="divider"></div>
 						<li onClick={chooseTopic}>
 							<NavLink to="articles/topics/football"> Football</NavLink>
 						</li>
-						<div className="divider"></div>
 						<li onClick={chooseTopic}>
 							<NavLink to="articles/topics/coding"> Coding</NavLink>
 						</li>
