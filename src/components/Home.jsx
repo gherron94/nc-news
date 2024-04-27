c0f320a86c723345008492b419aef1d7c79a3212
gherron94 <140562713+gherron94@users.noreply.github.com>
2024-04-27 23:16:46 +0100

    added photo and article button to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,13 @@ export default function Home() {
 	return (
 		<>
 			<h1>Welcome to NC News</h1>
+			<img
+				className="newspaper-img"
+				src={
+					"https://images.unsplash.com/photo-1585829365295-ab7cd400c167?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+				}
+				alt="a newspaper"
+			/>
 			<section className="home-section">
 				<p className="home-p">
 					Here you can find articles about Coding, Cooking and Football.{" "}
@@ -21,6 +28,7 @@ export default function Home() {
 					Don't forget to select your account in the profile section.
 				</p>
 			</section>
+			<button className="home-button">Click to view all articles</button>
 		</>
 	);
 }
